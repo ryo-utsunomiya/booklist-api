c5ad42b7cae552da43c8ac8d3557f39310ee04b0
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-06-03 17:01:22 +0900

    add mysql

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const mysql = require('mysql');
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: '',
+        password: '',
+        database: '',
+    });
+    connection.connect();
+
+    connection.query('SELECT * FROM books', (error, results, fields) => {
+        if (error) throw error;
+        console.log(results, fields);
+    });
+
+    connection.end();
 });
 
 app.listen(3000, () => {
