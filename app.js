ab26bf62d40c1c0538822ce57c21ec794aebcd7c
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-06-10 19:04:52 +0900

    add mysql

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const express = require('express');
-const mysql = require('mysql');
+const Book = require('./lib/Book');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: '',
-        password: '',
-        database: '',
-    });
-    connection.connect();
-
-    connection.query('SELECT * FROM books', (error, results, fields) => {
-        if (error) throw error;
-        console.log(results, fields);
-    });
-
-    connection.end();
+  Book.all().then((results) => {
+    res.json(results);
+  }).catch((error) => {
+    res.status(500);
+    res.json(error.message);
+  });
 });
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
